da92b3852f7449dbcbcd8f575ddd74b759bc5267
alan-f-eugenio <8264103+alan-f-eugenio@users.noreply.github.com>
2025-03-10 20:53:08 +0000

    style: github action auto-fix code style

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tighten\Ziggy\Ziggy;
@@ -42,7 +41,7 @@ class HandleInertiaRequests extends Middleware {
                 'user' => $request->user(),
                 'permissions' => $request->user()?->getPermissionsViaRoles()->pluck('name'),
             ],
-            'ziggy' => fn(): array => [
+            'ziggy' => fn (): array => [
                 ...(new Ziggy)->toArray(),
                 'location' => $request->url(),
             ],
