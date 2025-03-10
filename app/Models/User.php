290dc1b7af07ed23b77bd4e0efca59a7d79fa96b
Alan Eugenio <alan.f.eugenio@outlook.com>
2025-03-10 14:27:15 -0300

    feat: add casting to User created_at and update_at fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;
 
@@ -39,11 +38,12 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
+    protected function casts(): array {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'created_at' => 'datetime:m-d-Y H:i:s',
+            'updated_at' => 'datetime:m-d-Y H:i:s',
         ];
     }
 }
