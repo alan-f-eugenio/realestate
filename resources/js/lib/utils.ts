2b97f93890e06a76b540c8dde7803a007e040571
Alan Eugenio <alan.f.eugenio@outlook.com>
2025-03-10 16:01:28 -0300

    feat: add can util function to check if user has permissions

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function can(permissions: string[] | null, check: string | string[]) {
+    if (!permissions) return false;
+
+    const hasPermission = (permission: string, check: string) => {
+        const permissionParts = permission.split('.');
+        const checkParts = check.split('.');
+        if (permissionParts.length !== checkParts.length) return false;
+
+        return permissionParts.every((part, index) => part === '*' || part === checkParts[index]);
+    };
+
+    if (Array.isArray(check)) {
+        return check.some((chk) => permissions.some((perm) => hasPermission(perm, chk)));
+    } else {
+        return permissions.some((perm) => hasPermission(perm, check));
+    }
+}
