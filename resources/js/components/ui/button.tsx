6f08b591b2e4b3d92d7b44492f89439f0fe3980b
Alan Eugenio <alan.f.eugenio@outlook.com>
2025-03-11 22:19:03 -0300

    feat: add classes to force remove border, ring and ouline from button ghost variant

--- resources/js/components/ui/button.tsx
+++ resources/js/components/ui/button.tsx
@@ -21,7 +21,7 @@ const buttonVariants = cva(
           "border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground border-0",
+        ghost: "hover:bg-accent hover:text-accent-foreground border-0! ring-0! outline-0!",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
