92277ad1ea32cc73243fc0307d379c43d8605f63
Alan Eugenio <alan.f.eugenio@outlook.com>
2025-03-16 17:31:09 -0300

    reactor: change RolesAndPermissionsSeeder to use enums for permissions and roles

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Enums\PermissionsEnum;
+use App\Enums\RolesEnum;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
@@ -18,50 +20,25 @@ final class RolesAndPermissionsSeeder extends Seeder {
         // Reset cached roles and permissions
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-        Permission::create(['name' => 'users.view']);
-        Permission::create(['name' => 'users.viewAny']);
-        Permission::create(['name' => 'users.create']);
-        Permission::create(['name' => 'users.store']);
-        Permission::create(['name' => 'users.edit']);
-        Permission::create(['name' => 'users.update']);
-        Permission::create(['name' => 'users.destroy']);
-        Permission::create(['name' => 'users.assignRole.master']);
-        Permission::create(['name' => 'users.*']);
+        foreach (PermissionsEnum::cases() as $permissionsEnum) {
+            Permission::create(['name' => $permissionsEnum->value]);
+        }
 
-        Permission::create(['name' => 'properties.view']);
-        Permission::create(['name' => 'properties.viewAny']);
-        Permission::create(['name' => 'properties.create']);
-        Permission::create(['name' => 'properties.store']);
-        Permission::create(['name' => 'properties.edit']);
-        Permission::create(['name' => 'properties.update']);
-        Permission::create(['name' => 'properties.destroy']);
-        Permission::create(['name' => 'properties.*']);
+        $admin = Role::create(['name' => RolesEnum::ADMIN->value]);
+        $admin->givePermissionTo(PermissionsEnum::USERS_VIEW_ANY->value);
+        $admin->givePermissionTo(PermissionsEnum::USERS_CREATE->value);
+        $admin->givePermissionTo(PermissionsEnum::USERS_STORE->value);
+        $admin->givePermissionTo(PermissionsEnum::USERS_EDIT->value);
+        $admin->givePermissionTo(PermissionsEnum::USERS_UPDATE->value);
+        $admin->givePermissionTo(PermissionsEnum::USERS_DESTROY->value);
+        $admin->givePermissionTo(PermissionsEnum::PROPERTIES_ALL->value);
+        $admin->givePermissionTo(PermissionsEnum::SETTINGS_ALL->value);
 
-        Permission::create(['name' => 'settings.create']);
-        Permission::create(['name' => 'settings.store']);
-        Permission::create(['name' => 'settings.edit']);
-        Permission::create(['name' => 'settings.update']);
-        Permission::create(['name' => 'settings.*']);
+        $master = Role::create(['name' => RolesEnum::MASTER->value]);
+        $master->givePermissionTo(PermissionsEnum::USERS_ALL->value);
+        $master->givePermissionTo(PermissionsEnum::PROPERTIES_ALL->value);
+        $master->givePermissionTo(PermissionsEnum::SETTINGS_ALL->value);
 
-        // create roles and assign existing permissions
-        $admin = Role::create(['name' => 'admin']);
-        $admin->givePermissionTo('users.view');
-        $admin->givePermissionTo('users.viewAny');
-        $admin->givePermissionTo('users.create');
-        $admin->givePermissionTo('users.store');
-        $admin->givePermissionTo('users.edit');
-        $admin->givePermissionTo('users.update');
-        $admin->givePermissionTo('users.destroy');
-        $admin->givePermissionTo('properties.*');
-        $admin->givePermissionTo('settings.*');
-
-        $master = Role::create(['name' => 'master']);
-        $master->givePermissionTo('users.*');
-        $master->givePermissionTo('properties.*');
-        $master->givePermissionTo('settings.*');
-        // gets all permissions via Gate::before rule; see AuthServiceProvider
-
-        // create demo users
         $user = User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@user.com',
