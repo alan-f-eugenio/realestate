a84d199e42667cdacea1e1ffd1b9b4af4e2b19d2
Alan Eugenio <alan.f.eugenio@outlook.com>
2025-03-16 18:39:27 -0300

    feat: implement user authorization in UserController update and delete method with UserPolicy

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Inertia\Inertia;
 use Inertia\Response as InertiaResponse;
 use Spatie\Permission\Models\Role;
@@ -43,16 +44,11 @@ final class UserController extends Controller {
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(User $user): void {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      */
     public function edit(User $user): InertiaResponse {
+        Gate::authorize('update', $user);
+
         return Inertia::render('admin/users/user-create-edit', [
             'item' => [
                 ...$user->toArray(),
@@ -73,6 +69,8 @@ final class UserController extends Controller {
      * Remove the specified resource from storage.
      */
     public function destroy(User $user): RedirectResponse {
+        Gate::authorize('delete', $user);
+
         $user->delete();
 
         return back()->with('admin-flash', ['type' => 'success', 'text' => 'User deleted succesfully!']);
